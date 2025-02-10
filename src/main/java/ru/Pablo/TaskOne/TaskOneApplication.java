31a5b5411bf904d0b6bfee7da6065fd47e946f24
PavelBUTORIN <butorin_ps@mail.ru>
2025-02-10 03:16:26 +0300

    after mentor's comments

--- src/main/java/ru/Pablo/TaskOne/TaskOneApplication.java
+++ src/main/java/ru/Pablo/TaskOne/TaskOneApplication.java
@@ -2,11 +2,10 @@ package ru.Pablo.TaskOne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication // Важно!
-@ComponentScan("ru.Pablo.TaskOne")
+
+@SpringBootApplication
 public class TaskOneApplication {
 
 	public static void main(String[] args) {
